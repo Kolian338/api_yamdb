8f6d870718c04f992202344213520d7ee9a37efa
Vasily Zinovyev <zinavasa@gmail.com>
2023-12-12 17:14:32 +0300

    comment, review models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Reviews(models.Model):
+    author = models.ForeignKey(
+        'User', on_delete=models.CASCADE, related_name='reviews')
+    title = models.ForeignKey(
+        'Titles', on_delete=models.CASCADE, related_name='reviews')
+    text = models.TextField()
+    score = models.IntegerField()
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'title'],
+                name='repeated_review'
+            ),
+        ]
+
+
+class Comments(models.Model):
+    author = models.ForeignKey(
+        'User', on_delete=models.CASCADE, related_name='comments')
+    title = models.ForeignKey(
+        'Titles', on_delete=models.CASCADE, related_name='comments')
+    review = models.ForeignKey(
+        Reviews, on_delete=models.CASCADE, related_name='comments'
+    )
+    text = models.TextField()
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True)
