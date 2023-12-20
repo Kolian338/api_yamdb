9fb8044dfd7aae83493a96c275d73a4fdf9a546c
Vasily Zinovyev <zinavasa@gmail.com>
2023-12-20 11:59:04 +0300

    first review final

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -44,14 +44,10 @@ class IsAuthenticatedUser(BasePermission):
 
 class IsModerator(BasePermission):
     def has_permission(self, request, view):
-        return (request.method in SAFE_METHODS
-                or request.user.is_authenticated
-                and request.user.role == 'moderator')
+        return request.method in SAFE_METHODS or request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        return (request.method in SAFE_METHODS
-                or request.user.is_authenticated
-                and request.user.role == 'moderator')
+        return request.user.role == 'moderator'
 
 
 class IsAnonymous(BasePermission):
--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -80,12 +80,6 @@ class TitlesSerializer(BaseTilesSerializer):
     category = serializers.SlugRelatedField(queryset=Categories.objects.all(),
                                             slug_field='slug')
 
-    def create(self, validated_data):
-        return super().create(validated_data)
-
-    def update(self, instance, validated_data):
-        return super().update(instance, validated_data)
-
 
 class SignupSerializer(serializers.Serializer):
     """
